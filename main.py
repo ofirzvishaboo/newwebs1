8d7ffcb9f26582a381aba4c5802a1623bf399a96
Ofir <ofirfirst@gmail.com>
2023-06-30 01:17:01 +0300

    first solo project

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-#frfdfdfs
+import streamlit as st
+import pandas
 
-print("ggggg")
-print("hello")
+st.set_page_config(layout="wide")
+
+st.title("Ofir zvish")
+content = """
+    im 23 y/o from israel and grew up in givatayim
+    """
+st.info(content)
+
+col1, col2, col3 = st.columns(3)
+
+df = pandas.read_csv("data.csv", sep=",")
+with col1:
+    for index, row in df[:4].iterrows():
+        st.header(f'{row["first name"]} {row["last name"]}')
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.header(f'{row["first name"]} {row["last name"]}')
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+with col3:
+    for index, row in df[8:].iterrows():
+        st.header(f'{row["first name"]} {row["last name"]}')
+        st.write(row["role"])
+        st.image("images/" + row["image"])
